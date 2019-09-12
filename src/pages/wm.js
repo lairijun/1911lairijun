806426dd512bcd29825fcf56b2e6bfd15c6993eb
tu18701346619 <746274781@qq.com>
2019-09-12 17:37:34 +0800

    wm

---
+++ src/pages/wm.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react'
+class Wm extends Component{
+    render(){
+        return(
+            <div>
+                page吴茗
+            </div>
+        )
+    }
+}
+
+export default Wm
--- src/router.js
+++ src/router.js
@@ -3,15 +3,18 @@ import {HashRouter,NavLink,Switch,Redirect,Route} from 'react-router-dom'
 
 import Yifeng from './pages/zyf'
 import Hengyuan from './pages/hengyuan.js'
+import Wm from './pages/wm'
 class RootRouter extends Component{
     render(){
         return(
             <HashRouter>
                 <NavLink to='/hengyuan'>hengyuan</NavLink>
-                <NavLink to='/yifeng'>张益峰</NavLink>   
+                <NavLink to='/yifeng'>张益峰</NavLink> 
+                <NavLink to='/wuming'>吴茗</NavLink>  
                 <Redirect exact from='/' to='/hengyuan'></Redirect>
                 <Route path='/hengyuan' component={Hengyuan}></Route>
                 <Route path='/yifeng' component={Yifeng}></Route>
+                <Route path='/wuming' component={Wm}></Route>
             </HashRouter>
         )
     }
