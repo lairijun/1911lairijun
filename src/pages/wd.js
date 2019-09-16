ce5ec85246b71c86c062da10427b5d25ca2ee0eb
pluto-young <532384048@qq.com>
2019-09-16 20:51:47 +0800

    ok

---
+++ src/pages/wd.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react'
+class Wd extends Component{
+    render(){
+        return(
+            <div>
+                是傻逼
+            </div>
+        )
+    }
+}
+
+export default Wd
--- src/router.js
+++ src/router.js
@@ -4,17 +4,20 @@ import {HashRouter,NavLink,Switch,Redirect,Route} from 'react-router-dom'
 import Yifeng from './pages/zyf'
 import Hengyuan from './pages/hengyuan.js'
 import Wm from './pages/wm'
+import Wd from './pages/wd'
 class RootRouter extends Component{
     render(){
         return(
             <HashRouter>
                 <NavLink to='/hengyuan'>hengyuan</NavLink>
                 <NavLink to='/yifeng'>张益峰</NavLink> 
-                <NavLink to='/wuming'>吴茗</NavLink>  
+                <NavLink to='/wuming'>吴茗</NavLink>
+                <NavLink to='/weidong'>他们三个</NavLink>   
                 <Redirect exact from='/' to='/hengyuan'></Redirect>
                 <Route path='/hengyuan' component={Hengyuan}></Route>
                 <Route path='/yifeng' component={Yifeng}></Route>
                 <Route path='/wuming' component={Wm}></Route>
+                <Route path='/weidong' component={Wd}></Route>
             </HashRouter>
         )
     }
