08222201308e1269860b654efebdede6c50e63fc
1036157809 <1036157809@qq.com>
2019-09-17 17:50:33 +0800

    zyf

---
+++ src/components/topnav.js
@@ -0,0 +1,74 @@
+import React,{Component} from 'react'
+import {Menu,Icon} from 'antd'
+// import {Switch} from 'react-router-dom'
+import  './topnav.less'
+const {SubMenu}=Menu;
+ class Topnav extends Component{
+  state = {
+    current: 'mail',
+  };
+
+  handleClick = e => {
+    console.log('click ', e);
+    this.setState({
+      current: e.key,
+    });
+  };
+
+render(){
+    return(
+        <div className='topwarp'>
+       <Menu  className='top' onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
+        <Menu.Item key="search">
+          <Icon type="search" />
+          
+        </Menu.Item>
+
+        <Menu.Item key="down-circle" >
+          <Icon type="down-circle" />
+       
+        </Menu.Item>
+
+        <Menu.Item key="bell" >
+          <Icon type="bell" />
+       </Menu.Item>
+    
+ 
+             <SubMenu
+          title={
+            <div className="hand">
+             <div className='handimg'>
+               <img src='https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png'/>
+          </div>
+         
+ 
+            </div>
+          }
+        >
+        <Menu.Item key="user">
+          <Icon type='user' />
+          个人中心
+        </Menu.Item>
+
+        <Menu.Item key="setting" >
+          <Icon type="setting" />
+          个人设置
+        </Menu.Item>
+
+        <Menu.Item key="logout" >
+          <Icon type="logout" />
+          退出登录
+        </Menu.Item>
+        </SubMenu>
+
+        <Menu.Item key="global" >
+          <Icon type="global" />
+          </Menu.Item>
+      </Menu>
+        
+ 
+      </div>
+    )
+}
+ }
+ export default Topnav
--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react'
 import './index.less'
 import LeftNav from 'components/leftnav'
+import TopNav from 'components/topnav'
 class Admin extends Component{
     render(){
         return(
@@ -9,7 +10,7 @@ class Admin extends Component{
                 <LeftNav></LeftNav>
             </div>
             <div className='admin-right'>
-             <div className='admin-right-top'>top</div>
+             <div className='admin-right-top'><TopNav></TopNav></div>
              <div className='admin-right-center'>  {this.props.children}</div>
              <div className='admin-right-footer'>footer</div>
             </div>
